cb9a2c42b84b95b61ccbd9ebc3662abcf69a89b7
dilnazo4ka <64149769+dilnazo4ka@users.noreply.github.com>
2022-02-20 20:34:52 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var numberOfButtons = document.querySelectorAll("button").length;
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function (){
+        var audio = new Audio('click.mp3');
+        audio.play();
+    });
+}
+
+var numberOfInputs = document.querySelectorAll("input").length;
+for (var i = 0; i < numberOfInputs; i++) {
+    document.querySelectorAll("input")[i].addEventListener("keypress", function (){
+        var audio = new Audio('click.mp3');
+        audio.play();
+    });
+}
+function pushUpMessage() {
+    document.getElementById('sound').play();
+}
