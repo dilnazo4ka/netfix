e02ee643f2a5b7dab1b71c50016df183f268af37
dilnazo4ka <64149769+dilnazo4ka@users.noreply.github.com>
2022-02-21 01:36:51 +0600

    Update inde.js

--- inde.js
+++ inde.js
@@ -1,18 +1,19 @@
-var numberOfButtons = document.querySelectorAll("button").length;
-for (var i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function (){
-        var audio = new Audio('click.mp3');
-        audio.play();
-    });
-}
+document.querySelector('.musicBottom').onmouseover = function (e) {
+    var x = e.pageX - e.target.offsetLeft;
+    var y = e.pageY - e.target.offsetTop;
+    e.target.style.setProperty('--x', x + 'px');
+    e.target.style.setProperty('--y', y + 'px');
+};
 
-var numberOfInputs = document.querySelectorAll("input").length;
-for (var i = 0; i < numberOfInputs; i++) {
-    document.querySelectorAll("input")[i].addEventListener("keypress", function (){
-        var audio = new Audio('click.mp3');
-        audio.play();
-    });
-}
 function pushUpMessage() {
     document.getElementById('sound').play();
+}
+// document.addEventListener('keypress', function(){
+//     pushUpMessage();
+// });
+function playMusic(){
+    document.getElementById('thank').play();
+}
+function showModale() {
+    alert("Thank you!")
 }
